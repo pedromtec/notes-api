e586c7391bffe04d493485e59d22258b0998dccc
Pedro Soares <pedro.soares@pipefy.com>
2022-07-08 15:11:53 -0300

    Finish notes-api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,77 @@
-const http = require('http')
-const notes = require('./data/notes')
+const express = require('express')
+let notes = require('./data/notes')
+const app = express()
 
-// The code uses the createServer method of the http module to 
-// create a new web server. An event handler is registered to the 
-// server that is called every time an HTTP request is made to the 
-// server's address http://localhost:3001.
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
+app.use(express.json())
 
-  if (request.url === '/notes') {
-    return response.end(JSON.stringify(notes))
-  }
-  response.end(JSON.stringify({ home: 'Notes app home' }))
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
 })
 
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId()
+  }
+
+  notes = [...notes, note]
+
+  response.json(note)
+})
+
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
